39f18859361bcc20323dd3caa145b2967dd43387
KingAramis <entei.mail@protonmail.com>
2023-04-19 15:04:47 -0700

    Add basic page layout

---
+++ javascript.js
@@ -0,0 +1,27 @@
+/****************************** Library **************************************/ 
+
+let myLibrary = [];
+
+function Book() {
+    // constructor of book objects
+    // title
+    // author
+    // pages
+    // read or not
+}
+
+function addBookToLibrary() {
+    // called on submit
+    // invokes constructor
+    // takes data from form
+    // adds to myLibrary array
+}
+
+function displayMyLibrary() {
+    // loop through the library and display the book objects
+}
+
+function createNewBookContainer() {
+    // create and append a book container with the info of a book object
+    // 
+}
